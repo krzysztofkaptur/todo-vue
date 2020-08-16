4364318d26fa0ad92a39e47b6f7c1d1a2f13313b
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2020-08-16 08:56:18 +0200

    working version before refactor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,260 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+      <h1 class="header__title">todos</h1>
+    </header>
+    <main class="main">
+      <header class="header__form">
+        <button @click="markAllComplete" class="mark-as-completed__button"></button>
+        <form @submit.enter.prevent="addTodo" class="form">
+          <input type="text" v-model="todo" placeholder="What needs to be done?" class="form__input">
+        </form>
+      </header>
+      
+      <section class="todos">
+        <Todo 
+          v-for="todo in filteredTodos" 
+          :key="todo.id" 
+          :todo="todo" 
+          @deleteTodo="deleteTodo" 
+          @handleCheckbox="handleCheckbox"
+          @editTodo="editTodo" />
+      </section>
+
+      <section class="actions" v-if="todos.length > 0">
+        <span>{{ activeItemsCount }} items left</span>
+        <div>
+          <button @click="show = 'all'" class="actions__button" :class="{ 'active': show === 'all' }">All</button>
+          <button @click="show = 'active'" class="actions__button" :class="{ 'active': show === 'active' }">Active</button>
+          <button @click="show = 'completed'" class="actions__button" :class="{ 'active': show === 'completed' }">Completed</button>
+        </div>
+        <button @click="clearCompleted" class="actions__clear">Clear completed</button>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { v4 as uuidv4 } from 'uuid'
+
+import Todo from './components/Todo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todo
+  },
+  data() {
+    return {
+      todos: [],
+      todo: '',
+      show: 'all'
+    }
+  },
+  computed: {
+    filteredTodos() {
+      if(this.show === 'all') {
+        return this.todos
+      } else if(this.show === 'active') {
+        return this.todos.filter(todo => !todo.completed)
+      } else {
+        return this.todos.filter(todo => todo.completed)
+      }
+    },
+    activeItemsCount() {
+      return this.todos.filter(todo => !todo.completed).length
+    }
+  },
+  methods: {
+    getFromLocalStorage() {
+      return JSON.parse(localStorage.getItem('todos'))
+    },
+    setInLocalStorage() {
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    },
+    addTodo() {
+      const newTodo = {
+        id: uuidv4(),
+        title: this.todo,
+        completed: false
+      }
+
+      this.todos = [...this.todos, newTodo]
+      
+      this.setInLocalStorage()
+
+      this.clearInput()
+    },
+    clearInput() {
+      this.todo = ''
+    },
+    deleteTodo(id) {
+      const newTodos = this.todos.filter(todo => todo.id !== id)
+
+      this.updateTodos(newTodos)
+    },
+    handleCheckbox({ id, completed }) {
+      const newTodos = this.todos.map(todo => todo.id === id ? {...todo, completed} : todo)
+
+      this.updateTodos(newTodos)
+    },
+    markAllComplete() {
+      const notCompletedTodosCount = this.todos.filter(todo => !todo.completed).length
+
+      if(notCompletedTodosCount) {
+        const newTodos = this.todos.map(todo => ({ ...todo, completed: true }))
+        this.updateTodos(newTodos)
+      } else {
+        const newTodos = this.todos.map(todo => ({ ...todo, completed: false }))
+        this.updateTodos(newTodos)
+      }
+    },
+    clearCompleted() {
+      const newTodos = this.todos.filter(todo => !todo.completed)
+      
+      this.updateTodos(newTodos)
+    },
+    editTodo({ id, newTitle }) {
+      const newTodos = this.todos.map(todo => todo.id === id ? {...todo, title: newTitle} : todo)
+
+      this.updateTodos(newTodos)
+    },
+    updateTodos(arr) {
+      this.todos = arr
+      this.setInLocalStorage()
+    }
+  },
+  mounted() {
+    this.todos = this.getFromLocalStorage() || []
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.6rem;
+  background-color: #f5f5f5;
+}
+
+button {
+  cursor: pointer;
+  
+  &:focus {
+    outline: none;
+  }
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header {
+  &__title {
+    font-size: 10rem;
+    color: rgba(175, 47, 47, 0.15);
+    margin: 0;
+  }
+
+  &__form {
+    position: relative;
+  }
+}
+
+.main {
+  max-width: 55rem;
+  margin: auto;
+}
+
+.form {
+  &__input {
+    width: 100%;
+    padding: 1.6rem 1.6rem 1.6rem 6rem;
+    box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
+    border: none;
+    font-size: 2.4rem;
+
+    &::placeholder {
+      color: #E6E6E6;
+      font-style: italic;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+
+  &__button {
+    border: 1px solid transparent;
+    border-radius: 3px;
+    background: none;
+    padding: .3rem .7rem;
+    margin: .3rem;
+    
+    &.active {
+      border-color: rgba(175, 47, 47, 0.2);
+    }
+  }
+
+  &__clear {
+    border: none;
+    background: none;
+    border-bottom: 1px solid transparent;
+    transition: border-bottom .3s ease-in-out;
+
+    &:hover {
+      border-bottom-color: #000;
+    }
+  }
+}
+
+.mark-as-completed__button {
+  position: absolute;
+  width: 4rem;
+  height: 4rem;
+  left: 0;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: none;
+  border: none;
+
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 1.5rem;
+    height: .5rem;
+    background-color: #e6e6e6;
+  }
+
+  &:before {
+    transform: rotate(45deg) translate(-.3rem, .3rem);
+  }
+
+  &:after {
+    transform: rotate(-45deg)  translate(.3rem, .3rem);
+  }
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="todo" :class="{ 'todo--completed': todo.completed }">
+    <div v-if="!edit" class="todo__wrapper">
+      <input 
+      type="checkbox" 
+      class="todo__checkbox" 
+      :value="todo.completed" 
+      @change="handleCheckbox" 
+      :checked="todo.completed"
+      :id="todo.id">
+      <label :for="todo.id" class="todo__label"></label>
+      <h4 class="todo__title" @dblclick="editMode">{{ todo.title }}</h4>
+      <button @click="deleteTodo" class="todo__delete"></button>
+    </div>
+    <div v-else class="todo__edit">
+      <form @submit.enter.prevent="editTodo" class="todo__edit-form" @keydown.esc="edit = false">
+        <input type="text" :value="todo.title" @change="handleChange" class="todo__edit-form-input">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data() {
+    return {
+      edit: false,
+      newTodoTitle: ''
+    }
+  },
+  props: {
+    todo: {
+      type: Object
+    }
+  },
+  methods: {
+    deleteTodo() {
+      this.$emit('deleteTodo', this.todo.id)
+    },
+    handleCheckbox() {
+      this.$emit('handleCheckbox', { id: this.todo.id, completed: !this.todo.completed })
+    },
+    handleChange(e) {
+      this.newTodoTitle = e.target.value
+    },
+    editMode() {
+      this.edit = true
+    },
+    editTodo() {
+      if(this.newTodoTitle.trim()) {
+        this.$emit('editTodo', { id: this.todo.id, newTitle: this.newTodoTitle })
+        this.edit = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.todo {
+  $root: &;
+
+  border-bottom: 1px solid #ededed;
+  background-color: #fff;
+
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    position: relative;
+  }
+
+  &__title {
+    margin: 0;
+    width: 100%;
+    text-align: left;
+  }
+
+  &__checkbox {
+    display: none;
+
+    &:checked {
+      & ~ #{$root}__label {
+        &:before,
+        &:after {
+          opacity: 1;
+        }
+      }
+    }
+  }
+
+  &__label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 3rem;
+    height: 3rem;
+    border: 1px solid #ededed;
+    border-radius: 50%;
+    margin-right: 1rem;
+    position: relative;
+    flex: 0 0 3rem;
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      height: .2rem;
+      background-color: #5DC2AF;
+      opacity: 0;
+      transition: opacity .3s ease-in-out;
+    }
+
+    &:before {
+      transform: rotate(45deg) translate(-.2rem, 0.4rem);
+      width: .8rem;
+    }
+
+    &:after {
+      transform: rotate(-55deg) translate(0.2rem, 0.2rem);
+      width: 1.5rem;
+    }
+  }
+
+  &--completed {
+    #{$root} {
+      &__title {
+        text-decoration: line-through;
+      }
+    }
+  }
+
+  &__delete {
+    position: relative;
+    margin-left: auto;
+    width: 4rem;
+    height: 4rem;
+    border: none;
+    background: none;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    
+    &:before,
+    &:after {
+      content: '';
+      width: 1rem;
+      height: .2rem;
+      background-color: red;
+      position: absolute;
+      left: calc(50% - .5rem);
+    }
+
+    &:before {
+      transform: rotate(45deg);
+    }
+
+    &:after {
+      transform: rotate(-45deg);
+    }
+  }
+
+  &__edit {
+    padding: 1rem;
+
+    &-form {
+      display: flex;
+
+      &-input {
+        padding: 1rem 1.6rem .9rem 1rem;
+        width: 100%;
+        margin-left: 4rem;
+        flex: 1;
+      }
+    }
+  }
+
+  &:hover {
+    #{$root}__delete {
+      opacity: 1;
+    }
+  }
+}
+</style>
